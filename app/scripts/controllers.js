9fa4ab7231cf8b995d7fd2ba68e20a22421e4657
Aparus <rustamapaev@gmail.com>
2017-02-11 17:07:26 +0300

    code formatted

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,116 +1,118 @@
 'use strict';
 
 angular.module('quotesApp')
-        .controller('IndexController', ['$scope', 'quotesFactory', 
+    .controller('IndexController', ['$scope', 'quotesFactory',
         function($scope, quotesFactory) {
-            
-            quotesFactory.getQuotes().then(
-                function(response) {
-                    $scope.quotesText = response.data;
-                    
-                    $scope.quotes = quotesFactory.parseQuotes($scope.quotesText)
-                    $scope.dataPointsCandles = $scope.quotes.map(
-                                            function(elem, index){
-                                                return {x: new Date(elem.yyyy, elem.mm, elem.dd, elem.hh, elem.mn, elem.ss), y: [elem.open, elem.high, elem.low, elem.close]}
-        
-                                            })
-                    $scope.dataPointsVolumes = $scope.quotes.map(
-                                            function(elem, index){
-                                                return {x: new Date(elem.yyyy, elem.mm, elem.dd, elem.hh, elem.mn, elem.ss), y: elem.vol/10000000}
-        
-                                            })                    
-                    
-                    $scope.showCandlesStickAndVolumesChart($scope.dataPointsCandles, $scope.dataPointsVolumes)
-                    
-                }
-            );
-            
 
-    //creates and shows two charts , one above other 
-	$scope.showCandlesStickAndVolumesChart = function(dataPointsCandles, dataPointsVolumes) {
-        
-        $scope.chart1CandlesStick = new CanvasJS.Chart("chart1CandlesStickContainer",
-            {
-              zoomEnabled: true, 
-              zoomType: "xy",
-              axisX:{
-              tickLength: 0,
-              labelFormatter:function ( e ) {
-                       return " ";  
-                 },      
-              },
-              axisY:{
-               includeZero: false,      
-               labelFontSize: 10,
-               gridThickness:0
-              },
-              data: [
-              {
-                type: "candlestick",    
-                /*axisYType: "secondary",*/
-                dataPoints: $scope.dataPointsCandles
-              }
-              ], 
-            rangeChanged: syncHandler
-            });
+            quotesFactory.getQuotes()
+                .then(
+                    function(response) {
+                        $scope.quotesText = response.data;
 
-         $scope.chart1CandlesStick.render();
+                        $scope.quotes = quotesFactory.parseQuotes($scope.quotesText)
 
-         $scope.chart2Volumes = new CanvasJS.Chart("chart2VolumesContainer",
-            {
-              zoomEnabled: true, 
-              axisX:{
-                labelFontSize: 10,
-                labelAngle: 90
-              },
-              axisY:{
-                labelFontSize: 10,
-                gridThickness:0
-              },
-              data: [
-              {
-                type: "column",
-                /*axisYType: "secondary",*/
-                dataPoints: $scope.dataPointsVolumes
-              }
-              ], 
-            rangeChanged: syncHandler
-            });
+                        // we need two data sets: for Candles chart and Volumes chart, whith same dates by axisX and different by axisY
+                        $scope.dataPointsCandles = $scope.quotes.map(
+                            function(elem, index) {
+                                return {
+                                    x: new Date(elem.yyyy, elem.mm, elem.dd, elem.hh, elem.mn, elem.ss),
+                                    y: [elem.open, elem.high, elem.low, elem.close]
+                                }
 
-         $scope.chart2Volumes.render();
-        
-        
-    }
-        
-        // 
-        function syncHandler(e) {
-            
-            $scope.charts = [$scope.chart1CandlesStick, $scope.chart2Volumes]
-            
-            for (var i = 0; i < $scope.charts.length; i++) {
-                
-                var chart = $scope.charts[i];
+                            })
+                        $scope.dataPointsVolumes = $scope.quotes.map(
+                            function(elem, index) {
+                                return {
+                                    x: new Date(elem.yyyy, elem.mm, elem.dd, elem.hh, elem.mn, elem.ss),
+                                    y: elem.vol / 10000000
+                                }
 
-                if (!chart.options.axisX) chart.options.axisX = {};
+                            })
 
-                if (e.trigger === "reset") {
-                    chart.options.axisX.viewportMinimum = chart.options.axisX.viewportMaximum = null;
-                    chart.render();
+                        $scope.showCandlesStickAndVolumesChart($scope.dataPointsCandles, $scope.dataPointsVolumes)
 
-                } 
-                
-                else if (chart !== e.chart) {
-                    chart.options.axisX.viewportMinimum = e.axisX[0].viewportMinimum;
-                    chart.options.axisX.viewportMaximum = e.axisX[0].viewportMaximum;
-                    chart.render();
-                }
+                    }
+                );
+
+
+            //creates and shows two charts , one above another  
+            $scope.showCandlesStickAndVolumesChart = function(dataPointsCandles, dataPointsVolumes) {
+
+                $scope.chart1CandlesStick = new CanvasJS.Chart("chart1CandlesStickContainer", {
+                    zoomEnabled: true,
+                    zoomType: "xy",
+                    axisX: {
+                        tickLength: 0,
+                        labelFormatter: function(e) {
+                            return " ";
+                        },
+                    },
+                    axisY: {
+                        includeZero: false,
+                        labelFontSize: 10,
+                        gridThickness: 0
+                    },
+                    data: [{
+                        type: "candlestick",
+                        /*axisYType: "secondary",*/
+                        dataPoints: $scope.dataPointsCandles
+                    }],
+                    rangeChanged: syncHandler
+                });
+
+                $scope.chart1CandlesStick.render();
+
+                $scope.chart2Volumes = new CanvasJS.Chart("chart2VolumesContainer", {
+                    zoomEnabled: true,
+                    axisX: {
+                        labelFontSize: 10,
+                        labelAngle: 90
+                    },
+                    axisY: {
+                        labelFontSize: 10,
+                        gridThickness: 0
+                    },
+                    data: [{
+                        type: "column",
+                        /*axisYType: "secondary",*/
+                        dataPoints: $scope.dataPointsVolumes
+                    }],
+                    rangeChanged: syncHandler
+                });
+
+                $scope.chart2Volumes.render();
+
 
             }
-        }
-    
 
-            
-        }])
+            // syncronization between two charts: Candles and Volumes, when we zoom and go through them 
+            function syncHandler(e) {
+
+                $scope.charts = [$scope.chart1CandlesStick, $scope.chart2Volumes]
+
+                for (var i = 0; i < $scope.charts.length; i++) {
+
+                    var chart = $scope.charts[i];
+
+                    if (!chart.options.axisX) chart.options.axisX = {};
+
+                    if (e.trigger === "reset") {
+                        chart.options.axisX.viewportMinimum = chart.options.axisX.viewportMaximum = null;
+                        chart.render();
+
+                    } else if (chart !== e.chart) {
+                        chart.options.axisX.viewportMinimum = e.axisX[0].viewportMinimum;
+                        chart.options.axisX.viewportMaximum = e.axisX[0].viewportMaximum;
+                        chart.render();
+                    }
+
+                }
+            }
+
+
+
+        }
+    ])
 
 
 ;
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,51 +1,41 @@
 'use strict';
 
 angular.module('quotesApp')
-        .service('quotesFactory', ['$http', function($http) {
-                this.getQuotes = function(){
-                                        return $http.get("quotes/quotes.txt");
-                                    };
-            
-            
-            // 0<TICKER>;1<PER>;2<DATE>;3<TIME>;4<OPEN>;5<HIGH>;6<LOW>;7<CLOSE>;8<VOL>
-            //USD000000TOD;15;20161201;101500;63.8200000;63.8350000;63.4975000;63.5550000;171556000
-                this.parseQuotes = function(quotesTextCSV){
-                    var quotesLines = quotesTextCSV.split("\n");
-                    var quote = {}
-                    var quotes = []
-                    quotesLines.shift()
-                    quotesLines.pop()
-                    quotesLines.forEach(
-                                            function(quotes_line, index){
-                                                    quotes_line = quotes_line.split(";")
-                                                    //console.log(index, quotes_line)
-                                                    quote = {
-                                                                "ticker": quotes_line[0], 
-                                                                "per": parseFloat(quotes_line[1]), 
-                                                                "yyyy": parseFloat(quotes_line[2].substr(0,4)),
-                                                                "mm": parseFloat(quotes_line[2].substr(4,2))-1,
-                                                                "dd": parseFloat(quotes_line[2].substr(6,2)),
-                                                                "hh": parseFloat(quotes_line[3].substr(0,2)),
-                                                                "mn": parseFloat(quotes_line[3].substr(2,2)),
-                                                                "ss": parseFloat(quotes_line[3].substr(4,2)),
-                                                                "open": parseFloat(quotes_line[4]), 
-                                                                "high": parseFloat(quotes_line[5]), 
-                                                                "low": parseFloat(quotes_line[6]), 
-                                                                "close": parseFloat(quotes_line[7]), 
-                                                                "vol": parseFloat(quotes_line[8])
-                                                            }
-                                                    
-                                                    quotes.push(quote)
-                    
-                                            })
-                    
-                    
-                    return quotes
-                     
-                    
-                }
-                        
-        }])
+    .service('quotesFactory', ['$http', function($http) {
+        this.getQuotes = function() {
+            return $http.get("quotes/quotes.txt");
+        };
+        // 0<TICKER>;1<PER>;2<DATE>;3<TIME>;4<OPEN>;5<HIGH>;6<LOW>;7<CLOSE>;8<VOL>
+        //USD000000TOD;15;20161201;101500;63.8200000;63.8350000;63.4975000;63.5550000;171556000
+        this.parseQuotes = function(quotesTextCSV) {
+            var quotesLines = quotesTextCSV.split("\n");
+            var quote = {}
+            var quotes = []
+            quotesLines.shift()
+            quotesLines.pop()
+            quotesLines.forEach(
+                function(quotes_line, index) {
+                    quotes_line = quotes_line.split(";")
+                    //console.log(index, quotes_line)
+                    quote = {
+                        "ticker": quotes_line[0],
+                        "per": parseFloat(quotes_line[1]),
+                        "yyyy": parseFloat(quotes_line[2].substr(0, 4)),
+                        "mm": parseFloat(quotes_line[2].substr(4, 2)) - 1,
+                        "dd": parseFloat(quotes_line[2].substr(6, 2)),
+                        "hh": parseFloat(quotes_line[3].substr(0, 2)),
+                        "mn": parseFloat(quotes_line[3].substr(2, 2)),
+                        "ss": parseFloat(quotes_line[3].substr(4, 2)),
+                        "open": parseFloat(quotes_line[4]),
+                        "high": parseFloat(quotes_line[5]),
+                        "low": parseFloat(quotes_line[6]),
+                        "close": parseFloat(quotes_line[7]),
+                        "vol": parseFloat(quotes_line[8])
+                    }
+                    quotes.push(quote)
+                })
+            return quotes
+        }
 
-
+    }])
 ;
