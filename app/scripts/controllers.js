929a752906b1f974efbb4ec0de75c62a78db4add
Aparus <rustamapaev@gmail.com>
2017-02-11 12:28:17 +0300

    volumes chart added and syncronized with candlestick chart on zoom

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -9,47 +9,106 @@ angular.module('quotesApp')
                     $scope.quotesText = response.data;
                     
                     $scope.quotes = quotesFactory.parseQuotes($scope.quotesText)
-                    $scope.dataPoints = $scope.quotes.map(
+                    $scope.dataPointsCandles = $scope.quotes.map(
                                             function(elem, index){
                                                 return {x: new Date(elem.yyyy, elem.mm, elem.dd, elem.hh, elem.mn, elem.ss), y: [elem.open, elem.high, elem.low, elem.close]}
         
                                             })
+                    $scope.dataPointsVolumes = $scope.quotes.map(
+                                            function(elem, index){
+                                                return {x: new Date(elem.yyyy, elem.mm, elem.dd, elem.hh, elem.mn, elem.ss), y: elem.vol/10000000}
+        
+                                            })                    
                     
-                    $scope.showChart($scope.dataPoints)
+                    $scope.showCandlesStickAndVolumesChart($scope.dataPointsCandles, $scope.dataPointsVolumes)
+                    
                 }
             );
             
 
-    
-	$scope.showChart = function(dataPoints) {
+    //creates and shows two charts , one above other 
+	$scope.showCandlesStickAndVolumesChart = function(dataPointsCandles, dataPointsVolumes) {
         
-        $scope.chart = new CanvasJS.Chart("chartContainer",
-	       {
-                
-                title:{
-                    text: "Candlestick Chart for Quotes",
-                },
-                exportEnabled: true,
-                zoomEnabled: true, 
-                axisY: {
-                    includeZero: false,
-                    prefix: "$",
-                    labelFontSize: 10
-                },
-                axisX: {
-                    valueFormatString: "DD.MM.YYYY HH:MM:ss",
-                    labelAngle: 90, 
-                    labelFontSize: 10, 
-                },
-                data: [
-                {
-                    type: "candlestick", 
-                    dataPoints: dataPoints
-                }
-                ]
-	       });
-	$scope.chart.render(); 
+        $scope.chart1CandlesStick = new CanvasJS.Chart("chart1CandlesStickContainer",
+            {
+              zoomEnabled: true, 
+              zoomType: "xy",
+              axisX:{
+              tickLength: 0,
+              labelFormatter:function ( e ) {
+                       return " ";  
+                 },      
+              },
+              axisY:{
+               includeZero: false,      
+               labelFontSize: 10,
+               gridThickness:0
+              },
+              data: [
+              {
+                type: "candlestick",    
+                /*axisYType: "secondary",*/
+                dataPoints: $scope.dataPointsCandles
+              }
+              ], 
+            rangeChanged: syncHandler
+            });
+
+         $scope.chart1CandlesStick.render();
+
+         $scope.chart2Volumes = new CanvasJS.Chart("chart2VolumesContainer",
+            {
+              zoomEnabled: true, 
+              axisX:{
+                labelFontSize: 10,
+                labelAngle: 90
+              },
+              axisY:{
+                labelFontSize: 10,
+                gridThickness:0
+              },
+              data: [
+              {
+                type: "column",
+                /*axisYType: "secondary",*/
+                dataPoints: $scope.dataPointsVolumes
+              }
+              ], 
+            rangeChanged: syncHandler
+            });
+
+         $scope.chart2Volumes.render();
+        
+        
     }
+        
+        // 
+        function syncHandler(e) {
+            
+            $scope.charts = [$scope.chart1CandlesStick, $scope.chart2Volumes]
+            
+            for (var i = 0; i < $scope.charts.length; i++) {
+                
+                var chart = $scope.charts[i];
+
+                if (!chart.options.axisX) chart.options.axisX = {};
+
+                if (e.trigger === "reset") {
+                    chart.options.axisX.viewportMinimum = chart.options.axisX.viewportMaximum = null;
+                    chart.render();
+
+                } 
+                
+                else if (chart !== e.chart) {
+                    chart.options.axisX.viewportMinimum = e.axisX[0].viewportMinimum;
+                    chart.options.axisX.viewportMaximum = e.axisX[0].viewportMaximum;
+                    chart.render();
+                }
+
+            }
+        }
+    
+
             
         }])
 
